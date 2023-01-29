109035d71a5d51e82cbd6d3721975c7ab26de212
Prajwayne <46771166+Prajwayne@users.noreply.github.com>
2023-01-29 19:01:38 +0000

    Madlib project: Allows users to create their 3 line poem

---
+++ madlib.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Girrafe 
+
+public class madlibs
+{
+	public madlibs()
+	{
+		
+
+	}
+	static void Main(string[] args)
+    {
+		// We need three variable to store
+		string color, pluralNoun, Celebrity;
+		Console.Write("Enter a color");
+		color = Console.Readline();
+		Console.Write("Enter a Plural Noun");
+		pluralNoun = Console.Readline();
+		Console.Write("Enter a Celebrity");
+		Celebrity = Console.Readline();
+
+
+
+		// the project lets the user puts 3 wrods a build a poem
+		Console.WriteLine("Roses are"+color);
+		Console.WriteLine(pluralNoun+" are blue ");
+		Console.WriteLine("I love"+Celebrity);
+	}
+}
